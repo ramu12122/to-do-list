3fb84738ccf65c0bf8477784717c6bd635d946a4
ramu12122 <113298923+ramu12122@users.noreply.github.com>
2022-11-20 10:43:04 +0530

    latest

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,48 +1,45 @@
+let todoList = require("../todo");
+
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 /* eslint-disable no-undef */
-const todoList = require("../todo");
-const { all, markAsComplete, add, overdue, dueLater, dueToday } = todoList();
-describe("Todolist Test Suite", () => {
+describe("Todo List Test Suite", () => {
   beforeAll(() => {
+    // Seed the test data
+    [
+      {
+        title: "Pay rent",
+        completed: false,
+        dueDate: new Date().toLocaleDateString("en-CA"),
+      },
+    ].forEach(add);
+  });
+  test("Should add a new todo", () => {
+    expect(all.length).toEqual(1);
+
     add({
-      title: "Test Todo",
+      title: "A test item",
       completed: false,
       dueDate: new Date().toLocaleDateString("en-CA"),
     });
+
+    expect(all.length).toEqual(2);
   });
 
-  test("should add new todo", () => {
-    const count0 = all.length;
-    add({
-      title: "Test Todo",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
-    expect(all.length).toBe(count0 + 1);
-  });
-  test("should mark a todo as complete", () => {
-    expect(all[0].completed).toBe(false);
+  test("Should mark a todo as complete", () => {
+    expect(all[0].completed).toEqual(false);
     markAsComplete(0);
-    expect(all[0].completed).toBe(true);
+    expect(all[0].completed).toEqual(true);
   });
-  test("retrival of overdue", () => {
-    expect(overdue()).toStrictEqual(
-      all.filter(
-        (item) => item.dueDate < new Date().toLocaleDateString("en-CA")
-      )
-    );
+
+  test("Should retrieve overdue items", () => {
+    expect(overdue().length).toEqual(0);
   });
-  test("retrival of due today", () => {
-    expect(dueToday()).toStrictEqual(
-      all.filter(
-        (item) => item.dueDate === new Date().toLocaleDateString("en-CA")
-      )
-    );
+
+  test("Should retrieve due today items", () => {
+    expect(dueToday().length).toEqual(2);
   });
-  test("retrival of due later", () => {
-    expect(dueLater()).toStrictEqual(
-      all.filter(
-        (item) => item.dueDate > new Date().toLocaleDateString("en-CA")
-      )
-    );
+
+  test("Should retrieve due later items", () => {
+    expect(dueLater().length).toEqual(0);
   });
 });
